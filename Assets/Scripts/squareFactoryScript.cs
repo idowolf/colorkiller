80e3dc32797bf8fee7b5459e7a1138a6098479d7
tobisor <or.tobis@gmail.com>
2017-01-03 03:25:46 +0200

    adding square factories to level 1
    adding left and right factories, but without an square prefab - i
    couldnt check it.  tried to find it but with no success. (GREAT SUCCESS
    -NOT).
    i pretty sure it will be ok but for now the script gets a bullet prefab
    witch is not really efective

---
+++ Assets/Scripts/squareFactoryScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class squareFactoryScript : MonoBehaviour {
+
+    public GameObject enemy;
+    public float spawnTime = 3;
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("addEnemy", 0, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void addEnemy()
+    {
+        Renderer rd = GetComponent<Renderer>();
+        var y1 = transform.position.y - rd.bounds.size.y / 2;
+        var y2 = transform.position.y + rd.bounds.size.y / 2;
+        var spawnPoint = new Vector2( transform.position.x , Random.Range(y1, y2));
+        Instantiate(enemy, spawnPoint, Quaternion.identity);
+
+    }
+}
