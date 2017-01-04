bf7297f2b7e2367c3383bf8aeef896d5db046808
Ido Wolf <idowolf@gmail.com>
2017-01-04 23:02:21 +0200

    More fixes
    Added rotation for Android build.
    Added support for both Android and Unity Editor builds.
    Changed Unity Editor buttons to Left Arrow/Right Arrow instead of Q/W.

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -7,30 +7,34 @@ public class Rotate : MonoBehaviour {
 
     private float speed = 1000;
     public float rotSpeed = 2;
+    public float androidRotSpeed = 5;
     private GameObject endRotation;
-
     // Use this for initialization
     void Start ()
     {
         endRotation = new GameObject();
+#if UNITY_ANDROID
+        Input.compensateSensors = true;
+        Input.gyro.enabled = true;
+#endif
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void FixedUpdate ()
     {
-        if (Input.GetKey(KeyCode.Q))
+#if UNITY_ANDROID
+        this.transform.Rotate(0, 0, -Input.gyro.rotationRateUnbiased.z * androidRotSpeed);
+#endif
+#if UNITY_EDITOR
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             endRotation.transform.Rotate(Vector3.forward, rotSpeed, Space.World);
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             endRotation.transform.Rotate(Vector3.forward, -rotSpeed, Space.World);
         }
-
-
         this.transform.rotation = Quaternion.Lerp(this.transform.rotation, endRotation.transform.rotation, Time.deltaTime * speed);
+#endif
     }
-
-
-
 }
