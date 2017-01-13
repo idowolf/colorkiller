fb13c8905f0c1d1816ad422e92274319aa17a6b5
Ido Wolf <idowolf@gmail.com>
2017-01-13 17:28:45 +0200

    Done

--- Assets/Scripts/Destroyable.cs
+++ Assets/Scripts/Destroyable.cs
@@ -36,8 +36,6 @@ public class Destroyable : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (gameObject.GetComponent<ColoredObject>().color == other.gameObject.GetComponent<ColoredObject>().color)
-            initiateSelfDestruct = true;
         // get the point of contact
         ContactPoint2D contact = other.contacts[0];
 
