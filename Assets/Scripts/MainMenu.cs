269724e9eb2c3ed7de6c752bad14bc92ee551bbe
Ido Wolf <idowolf@gmail.com>
2017-01-15 01:25:12 +0200

    Revert "Minor tweaks"
    This reverts commit b27636d1b0f13810a8b4590355b72419b7ffe534.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -19,9 +19,20 @@ public class MainMenu : MonoBehaviour {
     // change scene when "PLAY" is pressed
     void OnTriggerEnter2D(Collider2D other)
     {
+
+        bool flg1 = false;
+        if (GetComponent<ColoredObject>() && other.GetComponent<ColoredObject>())
+
+            flg1 = BulletCtrl.isDestroyable(gameObject) && gameObject.GetComponent<ColoredObject>().color == other.gameObject.GetComponent<ColoredObject>().color;
+        bool flg2 = other.GetComponent<SpaceshipScript>();
+
+        bool flg3 = GetComponent<SpaceshipScript>() && other.GetComponent<EnemyScript>();
+        if (flg1 || flg2 || flg3)
+        {
             AudioSource audio = GetComponent<AudioSource>();
             audio.Play();
             SceneManager.LoadScene(sceneName);
+        }
     }
 
 
