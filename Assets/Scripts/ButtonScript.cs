f812a7e9c877f78e9409dd898b12d8ca7893f7a0
Kedeman <kedeman@hotmail.com>
2017-01-14 20:12:16 +0200

    Menu

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -17,8 +17,21 @@ public class ButtonScript : MonoBehaviour {
     {
         
     }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        SceneManager.LoadScene(sceneName);
+
+        bool flg1 = false;
+        if (GetComponent<ColoredObject>() && other.GetComponent<ColoredObject>())
+
+            flg1 = BulletCtrl.isDestroyable(gameObject) && gameObject.GetComponent<ColoredObject>().color == other.gameObject.GetComponent<ColoredObject>().color;
+        bool flg2 = other.GetComponent<SpaceshipScript>();
+
+        bool flg3 = GetComponent<SpaceshipScript>() && other.GetComponent<EnemyScript>();
+        if (flg1 || flg2 || flg3)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
+
