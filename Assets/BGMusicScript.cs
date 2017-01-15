eb69f1f478a98c43008da7f54a0c6923e37a70be
Ido Wolf <idowolf@gmail.com>
2017-01-15 15:41:14 +0200

    BGmusic and screen optimizations

---
+++ Assets/BGMusicScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BGMusicScript : MonoBehaviour {
+    static bool AudioBegin = false;
+    void Awake()
+    {
+        if (!AudioBegin)
+        {
+            GetComponent<AudioSource>().Play();
+            DontDestroyOnLoad(gameObject);
+            AudioBegin = true;
+        }
+    }
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "GameOver")
+        {
+            GetComponent<AudioSource>().Stop();
+            AudioBegin = false;
+        }
+    }
+
+}
